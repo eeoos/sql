e0b69970ba79bd0f67099f49a5afb546700bc354
jaehoonchoi <eeoos8840@gmail.com>
2026-06-02 14:35:16 +0900

    solve: Python 개발자 찾기

---
+++ programmers/select/276013.sql
@@ -0,0 +1,17 @@
+-- 문제: Python 개발자 찾기
+-- 링크: https://school.programmers.co.kr/learn/courses/30/lessons/276013
+
+SELECT
+    ID,
+    EMAIL,
+    FIRST_NAME,
+    LAST_NAME
+FROM
+    DEVELOPER_INFOS
+WHERE
+    SKILL_1 = 'Python'
+    OR SKILL_2 = 'Python'
+    OR SKILL_3 = 'Python'
+ORDER BY
+    ID
+;
