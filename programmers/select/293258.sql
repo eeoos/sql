8a5222ae0e80b0c49809d737ec028a0bbb529cb9
jaehoonchoi <eeoos8840@gmail.com>
2026-06-02 14:51:12 +0900

    solve: 잔챙이 잡은 수 구하기

---
+++ programmers/select/293258.sql
@@ -0,0 +1,10 @@
+-- 문제: 잔챙이 잡은 수 구하기
+-- 링크: https://school.programmers.co.kr/learn/courses/30/lessons/293258
+
+SELECT
+    COUNT(*) AS FISH_COUNT
+FROM
+    FISH_INFO
+WHERE
+    LENGTH IS NULL
+;
