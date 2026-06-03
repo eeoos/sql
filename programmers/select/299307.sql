5cca461b18d9fa36241449d2c9466db3a786c6e1
jaehoonchoi <eeoos8840@gmail.com>
2026-06-03 13:12:42 +0900

    solve: 대장균의 크기에 따라 분류하기 1

---
+++ programmers/select/299307.sql
@@ -0,0 +1,14 @@
+-- 문제: 대장균의 크기에 따라 분류하기 1
+-- 링크: https://school.programmers.co.kr/learn/courses/30/lessons/299307
+
+SELECT
+    ID,
+    (CASE WHEN SIZE_OF_COLONY > 1000 THEN 'HIGH'
+        WHEN SIZE_OF_COLONY > 100 THEN 'MEDIUM'
+        ELSE 'LOW'
+        END) AS SIZE
+FROM
+    ECOLI_DATA
+ORDER BY
+    ID
+;
