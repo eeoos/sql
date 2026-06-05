526378be96aa8a7e4daf3bab75dffa784d9734b1
jaehoonchoi <eeoos8840@gmail.com>
2026-06-05 19:48:03 +0900

    solve: 가격이 제일 비싼 식품의 정보 출력하기

---
+++ programmers/sum_max_min/131115.sql
@@ -0,0 +1,31 @@
+-- 문제: 가격이 제일 비싼 식품의 정보 출력하기
+-- 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131115
+
+SELECT
+    PRODUCT_ID,
+    PRODUCT_NAME,
+    PRODUCT_CD,
+    CATEGORY,
+    PRICE
+FROM
+    FOOD_PRODUCT
+WHERE PRICE = (
+    SELECT
+        MAX(PRICE)
+    FROM
+        FOOD_PRODUCT
+    )
+;
+
+SELECT
+    PRODUCT_ID,
+    PRODUCT_NAME,
+    PRODUCT_CD,
+    CATEGORY,
+    PRICE
+FROM
+    FOOD_PRODUCT
+ORDER BY
+    PRICE DESC
+LIMIT
+    1;
