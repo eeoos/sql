18dde0f4d846e3114463601ff99e142962e75904
jaehoonchoi <eeoos8840@gmail.com>
2026-06-07 14:44:22 +0900

    solve: 성분으로 구분한 아이스크림 총 주문량

---
+++ programmers/group_by/133026.sql
@@ -0,0 +1,15 @@
+-- 문제: 성분으로 구분한 아이스크림 총 주문량
+-- 링크: https://school.programmers.co.kr/learn/courses/30/lessons/133026
+
+SELECT
+    I.INGREDIENT_TYPE,
+    SUM(F.TOTAL_ORDER) AS TOTAL_ORDER
+FROM
+    FIRST_HALF F
+JOIN
+    ICECREAM_INFO I ON F.FLAVOR = I.FLAVOR
+GROUP BY
+    I.INGREDIENT_TYPE
+ORDER BY
+    TOTAL_ORDER
+;
