17e6c49822938d4ba1b6f4b90897eba72c832714
jaehoonchoi <eeoos8840@gmail.com>
2026-06-08 16:03:13 +0900

    solve: 조건에 맞는 사용자와 총 거래금액 조회하기

---
+++ programmers/group_by/164668.sql
@@ -0,0 +1,21 @@
+-- 문제: 조건에 맞는 사용자와 총 거래금액 조회하기
+-- 링크: https://school.programmers.co.kr/learn/courses/30/lessons/164668
+
+SELECT
+    U.USER_ID,
+    U.NICKNAME,
+    SUM(PRICE) AS TOTAL_SALES
+FROM
+    USED_GOODS_BOARD B
+JOIN
+    USED_GOODS_USER U ON B.WRITER_ID = U.USER_ID
+WHERE
+    B.STATUS = 'DONE'
+GROUP BY
+    U.USER_ID,
+    U.NICKNAME
+HAVING
+    TOTAL_SALES >= 700000
+ORDER BY
+    TOTAL_SALES
+;
