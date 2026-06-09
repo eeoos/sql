6200776e147b1b38453e05007a31f093cc2ae8d1
jaehoonchoi <eeoos8840@gmail.com>
2026-06-09 11:00:49 +0900

    solve: 동명 동물 수 찾기

---
+++ programmers/group_by/59041.sql
@@ -0,0 +1,17 @@
+-- 문제: 동명 동물 수 찾기
+-- 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59041
+
+SELECT
+    NAME,
+    COUNT(*) AS COUNT
+FROM
+    ANIMAL_INS
+WHERE
+    NAME IS NOT NULL
+GROUP BY
+    NAME
+HAVING
+    COUNT(*) >= 2
+ORDER BY
+    NAME
+;
