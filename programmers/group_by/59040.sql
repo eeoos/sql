8bbd6935e3b4daec72adfb04aa0e954e3ffd2b19
jaehoonchoi <eeoos8840@gmail.com>
2026-06-09 10:52:05 +0900

    solve: 고양이와 개는 몇 마리 있을까

---
+++ programmers/group_by/59040.sql
@@ -0,0 +1,15 @@
+-- 문제: 고양이와 개는 몇 마리 있을까
+-- 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59040
+
+SELECT
+    ANIMAL_TYPE,
+    COUNT(*) AS count
+FROM
+    ANIMAL_INS
+WHERE
+    ANIMAL_TYPE IN ('Cat', 'Dog')
+GROUP BY
+    ANIMAL_TYPE
+ORDER BY
+    ANIMAL_TYPE
+;
