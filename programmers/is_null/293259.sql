3d3e065fd59741b6740f7d63766dcc6a87c7f69a
jaehoonchoi <eeoos8840@gmail.com>
2026-06-11 12:49:50 +0900

    solve: 잡은 물고기의 평균 길이 구하기

---
+++ programmers/is_null/293259.sql
@@ -0,0 +1,8 @@
+-- 문제: 잡은 물고기의 평균 길이 구하기
+-- 링크: https://school.programmers.co.kr/learn/courses/30/lessons/293259
+
+SELECT
+    ROUND(AVG(COALESCE(LENGTH, 10)), 2) AS AVERAGE_LENGTH
+FROM
+    FISH_INFO
+;
