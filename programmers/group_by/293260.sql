0e1f9eb92fa6bd7e46f69d670fc46b47ed691beb
jaehoonchoi <eeoos8840@gmail.com>
2026-06-11 11:07:24 +0900

    solve: 월별 잡은 물고기 수 구하기

---
+++ programmers/group_by/293260.sql
@@ -0,0 +1,13 @@
+-- 문제: 월별 잡은 물고기 수 구하기
+-- 링크: https://school.programmers.co.kr/learn/courses/30/lessons/293260
+
+SELECT
+    COUNT(*) AS FISH_COUNT,
+    MONTH(TIME) AS `MONTH`
+FROM
+    FISH_INFO
+GROUP BY
+    MONTH(TIME)
+ORDER BY
+    MONTH(TIME)
+;
