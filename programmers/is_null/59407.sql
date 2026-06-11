bdf80532fe4cc8a13345b5999a8632b0ae335f89
jaehoonchoi <eeoos8840@gmail.com>
2026-06-11 11:39:22 +0900

    solve: 이름이 있는 동물의 아이디

---
+++ programmers/is_null/59407.sql
@@ -0,0 +1,12 @@
+-- 문제: 이름이 있는 동물의 아이디
+-- 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59407
+
+SELECT
+    ANIMAL_ID
+FROM
+    ANIMAL_INS
+WHERE
+    NAME IS NOT NULL
+ORDER BY
+    ANIMAL_ID
+;
