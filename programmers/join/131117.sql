9518978e1bbd5975f4dc5162a94d84512b1f15ac
jaehoonchoi <eeoos8840@gmail.com>
2026-06-12 13:39:43 +0900

    solve: 5월 식품들의 총매출 구하기

---
+++ programmers/join/131117.sql
@@ -0,0 +1,20 @@
+-- 문제: 5월 식품들의 총매출 조회하기
+-- 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131117
+
+SELECT
+    P.PRODUCT_ID,
+    P.PRODUCT_NAME,
+    SUM(P.PRICE * O.AMOUNT) AS TOTAL_SALES
+FROM
+    FOOD_PRODUCT P
+JOIN
+    FOOD_ORDER O ON P.PRODUCT_ID = O.PRODUCT_ID
+WHERE
+    O.PRODUCE_DATE >= '2022-05-01' AND O.PRODUCE_DATE < '2022-06-01'
+GROUP BY
+    P.PRODUCT_ID,
+    P.PRODUCT_NAME
+ORDER BY
+    TOTAL_SALES DESC,
+    P.PRODUCT_ID
+;
