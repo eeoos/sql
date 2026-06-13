03d836a4a863bae630713503473aef0c2e8425bc
jaehoonchoi <eeoos8840@gmail.com>
2026-06-13 15:57:50 +0900

    solve: 있었는데요 없었습니다

---
+++ programmers/join/59043.sql
@@ -0,0 +1,15 @@
+-- 문제: 있었는데요 없었습니다
+-- 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59043
+
+SELECT
+    I.ANIMAL_ID,
+    I.NAME
+FROM
+    ANIMAL_INS I
+        JOIN
+    ANIMAL_OUTS O ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE
+    I.DATETIME > O.DATETIME
+ORDER BY
+    I.DATETIME
+;
