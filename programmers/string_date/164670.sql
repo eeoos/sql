17d12466dee9c15060ad4add256a4ed44610e7cb
jaehoonchoi <eeoos8840@gmail.com>
2026-06-14 13:53:45 +0900

    solve: 조건에 맞는 사용자 정보 조회하기

---
+++ programmers/string_date/164670.sql
@@ -0,0 +1,29 @@
+-- 문제: 조건에 맞는 사용자 정보 조회하기
+-- 링크: https://school.programmers.co.kr/learn/courses/30/lessons/164670
+
+SELECT
+    USER_ID,
+    NICKNAME,
+    CONCAT_WS(' ', CITY, STREET_ADDRESS1, STREET_ADDRESS2) AS `전체주소`,
+    CONCAT_WS(
+            '-',
+            SUBSTRING(TLNO, 1, 3),
+            SUBSTRING(TLNO, 4, 4),
+            SUBSTRING(TLNO, 8, 4)
+    ) AS `전화번호`
+FROM
+    USED_GOODS_USER
+WHERE
+    USER_ID IN (
+        SELECT
+            WRITER_ID
+        FROM
+            USED_GOODS_BOARD
+        GROUP BY
+            WRITER_ID
+        HAVING
+            COUNT(*) >= 3
+    )
+ORDER BY
+    USER_ID DESC
+;
