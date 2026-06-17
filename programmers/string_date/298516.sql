5f19559b99559c1135f99f6d2be3d9fa8d4670a6
jaehoonchoi <eeoos8840@gmail.com>
2026-06-17 14:27:14 +0900

    solve: 한 해에 잡은 물고기 수 구하기

---
+++ programmers/string_date/298516.sql
@@ -0,0 +1,10 @@
+-- 문제: 한 해에 잡은 물고기 수 구하기
+-- 링크: https://school.programmers.co.kr/learn/courses/30/lessons/298516
+
+SELECT
+    COUNT(*) AS FISH_COUNT
+FROM
+    FISH_INFO
+WHERE
+    YEAR(TIME) = 2021
+;
